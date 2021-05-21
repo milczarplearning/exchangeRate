d61e14abfd9763b59703657464a25775e70c7abd
milczarp <milczarplearning@gmail.com>
2021-05-21 07:50:39 +0200

    Removed unnecesseary unique index

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS exchange_rate;
 CREATE TABLE exchange_rate
 (
-    date_time timestamp NOT NULL UNIQUE,
-    rate decimal NOT NULL
+    date_time timestamp NOT NULL,
+    rate      decimal   NOT NULL
 );
 CREATE INDEX date_time_index ON exchange_rate(date_time);
